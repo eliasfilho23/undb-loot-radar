9361d71c6461b5987e5bf43c9723c982ebea6704
lucas gabriel penha <lucas093@icloud.com>
2026-04-08 22:53:37 -0300

    Create install-dependencies.sh

---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo "Installing dependencies..."
+npm install
+
+echo "Installing shared dependencies..."
+cd $SCRIPT_DIR/../shared && npm install && npm run build
+
+echo "Installing frontend dependencies..."
+cd $SCRIPT_DIR/../frontend && npm install
+
+echo "Installing backend dependencies..."
+cd $SCRIPT_DIR/../backend && npm install
+
+echo "Done!"
