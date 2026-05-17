397ad92bd54d36e128da165e01a80b42ad0b82ae
lucas gabriel penha <lucas093@icloud.com>
2026-05-17 19:22:43 -0300

    Delete backend/src/api/games/GamesApiModule.ts

--- backend/src/api/games/GamesApiModule.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GamesApiController } from './GamesApiController';
-import { GamesApiService } from './GamesApiService';
-
-@Module({
-  controllers: [ GamesApiController ],
-  providers  : [ GamesApiService ],
-})
-export class GamesApiModule {}
